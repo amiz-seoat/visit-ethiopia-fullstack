46d201bc87c562b179437ebf3b99c532bbb37b68
AbyalewLobe <labyalew@gmail.com>
2025-09-21 09:16:22 -0700

    ...

--- visit-ethiopia-backend-defence/server.js
+++ visit-ethiopia-backend-defence/server.js
@@ -1,15 +1,20 @@
-import dotenv from 'dotenv';
-import app from './app.js';
-import connectDB from './config/db.js';
+import dotenv from 'dotenv'
+import app from './app.js'
+import connectDB from './config/db.js'
 
 if (process.env.NODE_ENV !== 'production') {
-  dotenv.config({ path: './config.env' });
+  dotenv.config({ path: './config.env' })
 }
 
+// ✅ Use environment variable for port with a fallback
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+})
 
 // ✅ Connect to MongoDB once when the function is initialized
-connectDB();
-
+connectDB()
 
 // ✅ Export app instead of listening to a port
-export default app;
+export default app
